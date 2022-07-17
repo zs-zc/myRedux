2549eec86bcfa73d9593dc2ed6a92566560c96cd
zs-zc <3011121187@qq.com>
2022-07-17 21:02:01 +0800

    fix:优化代码（api）

--- src/containers/count/index.jsx
+++ src/containers/count/index.jsx
@@ -2,21 +2,27 @@ import CountUI from "../../components/count"
 import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from "../../redux/count_action"
 
 import { connect } from "react-redux"
-function mapStateToProps(state) {
-    return { count: state }
-}
-function mapDispatchToProps(dispatch) {
-    return {
-        increment: (data) => {
-            dispatch(createIncrementAction(data))
-        },
-        decrement: (data) => {
-            dispatch(createDecrementAction(data))
-        },
-        incrementAsync: (data, time) => {
-            dispatch(createIncrementAsyncAction(data, time))
-        }
+// function mapStateToProps(state) {
+//     return { count: state }
+// }
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         increment: (data) => {
+//             dispatch(createIncrementAction(data))
+//         },
+//         decrement: (data) => {
+//             dispatch(createDecrementAction(data))
+//         },
+//         incrementAsync: (data, time) => {
+//             dispatch(createIncrementAsyncAction(data, time))
+//         }
 
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
+//     }
+// }
+// export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
+
+export default connect(state => ({ count: state }), {
+    increment: createIncrementAction,
+    decrement: createDecrementAction,
+    incrementAsync: createIncrementAsyncAction
+})(CountUI)
