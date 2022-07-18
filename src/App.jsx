536fb4da8574e99f599a5b0723cf01459c1ff23c
zs-zc <3011121187@qq.com>
2022-07-18 21:09:27 +0800

    fix:redux文件夹优化

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Count from "../src/containers/count"
+import Person from './containers/Person'
 import store from "../src/redux/store"
 export default class App extends Component {
     render() {
         return (
             <div>
                 <Count store={store} />
+                <hr />
+                <Person />
             </div>
         )
     }
